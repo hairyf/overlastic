3a138928cddd51a0780f863aa5e43d08ffa53fc6
mrmao <wwu710632@gmail.com>
2023-04-26 11:15:16 +0800

    feat: add React related testing tools

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
-
+import React from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     Vue(),
     VueJsx(),
+    React() as any,
   ],
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './__tests__/test/setup.ts',
   },
 })
