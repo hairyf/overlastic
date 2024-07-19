2f18e8360c76bd7c9292a3d386039319bdd9b9ea
Mr.Mao <951416545@qq.com>
2024-07-19 14:40:03 +0800

    chore: remove install provider

--- packages/@vue/__tests__/install.spec.ts
+++ packages/@vue/__tests__/install.spec.ts
@@ -1,35 +1,12 @@
 import { mount } from '@vue/test-utils'
-import { defineComponent, h } from 'vue'
-import { Provider, install } from '../src'
+import { install } from '../src'
 import { context } from '../src/internal'
 import Basic from './components/basic.vue'
 
 describe('@overlastic/vue:install', () => {
-  it('install:provider', () => {
-    const wrapper = mount(Provider)
-
-    expect(context.appContext).not.toBeUndefined()
-
-    context.appContext = null
-    wrapper.unmount()
-  })
-  it('install:provider:content', () => {
-    const Main = defineComponent({
-      render() {
-        return h(Provider, {}, {
-          default: () => 'content',
-        })
-      },
-    })
-    const wrapper = mount(Main)
-    expect(wrapper.text()).toBe('content')
-
-    wrapper.unmount()
-  })
-
   it('install:function', () => {
     const wrapper = mount(Basic, {
-      global: { plugins: [install] },
+      global: { plugins: [install as any] },
     })
 
     expect(context.appContext).not.toBeUndefined()
